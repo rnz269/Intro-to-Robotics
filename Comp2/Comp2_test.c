c923e4df11906b6c516c32dab272a29e479d100b
U-DPU\dganama <dganama@CDM-DA503LAB04.dpu.depaul.edu>
2018-11-09 20:11:14 -0600

    Working Version Daoud

--- Comp2/Comp2_test.c
+++ Comp2/Comp2_test.c
@@ -1,28 +1,77 @@
 #pragma config(StandardModel, "EV3_REMBOT")
 
-
-float distanceToObstacle = 0;
+const int Scanning_State = 0;
+const int Charging_State = 1;
+const int DetectedEdge_State = 2;
+//const int Defense_State = 3;
+const int Attacking_State = 4;
+int current_State;
+const int max_Dist = 40;
+const int attack_Dist = 10;
+int distanceToObstacle = 0;
+int colorRead;
 float rateRead = 0;
-int colorRead = 0;
+int Attack_Power = 50;
+int squigglyMotionConstant;
+const int white_color_val = 47;
+const int black_color_val = 6;
+const int colorThreshold = 10;
 void stopMotors()
 {
 	setMotorSpeed(motorB, 0);
 	setMotorSpeed(motorC, 0);
 }
-task colorSensorTask()
+
+void back_up()
+{
+	stopMotors();
+	setMotorSync(motorB,motorC, 30, -50);
+	sleep(1000);
+}
+void scanForOpponent(){
+	setMotorSync(motorB,motorC, 100, 20);
+}
+void charge_opponent()
+{
+	setMotorSync(motorB, motorC, squigglyMotionConstant, 50);
+}
+task squigglyMotionController()
+{
+	squigglyMotionConstant = 10;
+	while(true){
+		squigglyMotionConstant*=(-1);
+		sleep(500);
+		}
+}
+/*
+task attackPowerController()
 {
 	while(true)
 	{
-			colorRead = getColorReflected(S3);
+		if(current_State==Attacking_State){
+			Attack_Power+=10;
+			sleep(500);
+		}
+		else
+			Attack_Power = 50;
 	}
+}*/
+void attack_opponent()
+{
+	setMotorSync(motorB, motorC, 0, Attack_Power);
+}
+
+task colorSensorTask()
+{
+	sensorReset(S2);
+	while(true)
+			colorRead = getColorReflected(S3);
 }
 task RangeSensorTask()
 {
 	sensorReset(S4);
 	while(true)
-	{
 		distanceToObstacle = getUSDistance(S4);
-	}
 }
 task gyroRatingReader()
 {
@@ -30,28 +79,73 @@ task gyroRatingReader()
 	while(true)
 		rateRead = getGyroRate(S2);
 }
+task colorStateController()
+{
+	while(true)
+		if(current_State==Scanning_State)
+			if(colorRead < (black_color_val + colorThreshold))
+				current_State = DetectedEdge_State;
+}
+task distanceStateController()
+{
+	while(true)
+	{
+		if( current_State != DetectedEdge_State )
+		{
+			if(distanceToObstacle < attack_Dist)
+				current_State = Attacking_State;
+			else if(distanceToObstacle < max_Dist)
+				current_State = Charging_State;
+			else
+				current_State = Scanning_State;
+		}
+	}
+}
+task MotionController()
+{
+	current_State = Scanning_State;
+	while (true)
+	{
+		switch(current_State){
+			case DetectedEdge_State:
+				back_up();
+				current_State = Scanning_State;
+				break;
+			case Charging_State:
+				charge_opponent();
+				break;
+			case Attacking_State:
+				attack_opponent();
+				break;
+			default:
+				scanForOpponent();
+				break;
+		}
+	}
+}
 task displayTask()
 {
 	while(true)
 	{
 		displayTextLine(1,"Color Read: %d", colorRead);
-		//displayTextLine(2,"motor B Power: %f", motorB_Power);
-		displayTextLine(3,"Gyro rate: %f", rateRead);
+		displayTextLine(2,"current state: %d", current_State);
+		displayTextLine(3,"attack power: %d", Attack_Power);
 		displayTextLine(4,"Range to Obstacle: %f", distanceToObstacle);
 	}
 }
 task main()
 {
-	startTask(displayTask);
+	colorRead = 100;
+	startTask(colorSensorTask);
 	startTask(gyroRatingReader);
 	startTask(RangeSensorTask);
-	startTask(colorSensorTask);
-	int speed = 50;
-	//int direction = 50;
+	startTask(displayTask);
+	startTask(colorStateController);
+	startTask(distanceStateController);
+	startTask(squigglyMotionController);
+	startTask(attackPowerController);
+	startTask(MotionController);
 	while(true){
-		sleep(7000);
-		//stopMotors();
-		//setMotorSync(motorB,motorC, direction, speed);
-		//direction = -direction;
+		sleep(1000);
 	}
 }
--- Comp2/Juve_comp2_try1.c
+++ Comp2/Juve_comp2_try1.c
@@ -1,40 +1,33 @@
 #pragma config(StandardModel, "EV3_REMBOT")
 
-
 // States
 const int Scanning_State = 0;
 const int Charging_State = 1;
 const int DetectedEdge_State = 2;
-const int Defense_State = 3;
+//const int Defense_State = 3;
 const int Attacking_State = 4;
 int current_State;
 
 // Sensors
 const int max_Dist = 40;
-int distanceToObstacle = 0;
-int colorRead = 0;
-
-const int white_color_val = 47;
-const int black_color_val = 6;
-
+const int attack_Dist = 10;
+int distanceToObstacle = 100;
+int colorRead;
+float rateRead;
+const int white_color_val = 87;
+const int black_color_val = 15;
+const int colorThreshold = 20;
 
 // Motors
-int extraTurnPower = 5;
-int motorB_Power;
-int motorC_Power;
-
-
-bool noObstacle = true;
-bool alreadyTurning = false;
-float degreeReading;
-
+int squigglyMotionConstant = 10;
+int Attack_Power = 100;
 
 void stopMotors()
 {
 	setMotorSpeed(motorB, 0);
 	setMotorSpeed(motorC, 0);
 }
-
+//********* Sensor Tasks ***********
 task colorSensorTask()
 {
 	while(true)
@@ -47,53 +40,108 @@ task RangeSensorTask()
 	while(true)
 		distanceToObstacle = getUSDistance(S4);
 }
-void back_up()
-{
-	stopMotors();
-	setMotorSync(motorB,motorC, 100, 100);
-	sleep(1000);
-}
 
-task gyroDegreeReader()
+task gyroRatingReader()
 {
 	resetGyro(S2);
 	while(true)
-		degreeReading = getGyroDegrees(S2);
+		rateRead = getGyroRate(S2);
 }
 
-// Write Squiggly Movement
+// *********** State Functions ***********
+
+// At edge detected state
+void back_up()
+{
+	stopMotors();
+	setMotorSync(motorB,motorC, 30, -50);
+	sleep(1000);
+}
+
+// At scanning state
+void scanForOpponent()
+{
+	setMotorSync(motorB,motorC, 100, 20);
+}
+
+// At charging state
 void charge_opponent()
 {
-	setMotorSync(motorB,motorC, 0, 50);
+	setMotorSync(motorB, motorC, squigglyMotionConstant, 50);
 }
-task MotionController()
+
+// At attacking state
+void attack_opponent()
 {
-	while (true)
+	setMotorSync(motorB,motorC, 0, Attack_Power);
+}
+
+// Attack Power Controller if we decide that increasing power is better than straight
+/*
+task attackPowerController()
+{
+	while(true)
 	{
-		switch(current_State){
-			case Charging_State:
-				charge_opponent();
-				break;
-			case DetectedEdge_State:
-				back_up();
-				break;
-			default:
-				charge_opponent();
-				//setMotorSync(motorB,motorC, 100, 25);
-				break;
+		if(current_State==Attacking_State){
+			Attack_Power+=10;
+			sleep(500);
+		}
+		else
+			Attack_Power = 50;
+	}
+}*/
+// Switch direction between right and left
+task squigglyMotionController()
+{
+	squigglyMotionConstant = 10;
+	while(true){
+		squigglyMotionConstant*=(-1);
+		sleep(500);
+		}
+}
+task colorStateController()
+{
+	while(true)
+		// If the current state is charging or scanning and found age go state detected edge
+		if(current_State < 2)
+			if(colorRead < (black_color_val + colorThreshold))
+				current_State = DetectedEdge_State;
+}
+task distanceStateController()
+{
+	while(true)
+	{
+		if( current_State != DetectedEdge_State )
+		{
+			if(distanceToObstacle < attack_Dist)
+				current_State = Attacking_State;
+			else if(distanceToObstacle < max_Dist)
+				current_State = Charging_State;
+			else
+				current_State = Scanning_State;
 		}
 	}
 }
-task StateController()
+task MotionController()
 {
+	current_State = Scanning_State;
 	while (true)
 	{
-		if( distanceToObstacle < max_Dist)
-			current_State = Charging_State;
-		else if ( colorRead < (black_color_val + 10))
-			current_State = DetectedEdge_State;
-		else
-			current_State = 0;
+		switch(current_State){
+			case DetectedEdge_State:
+				back_up();
+				current_State = Scanning_State;
+				break;
+			case Charging_State:
+				charge_opponent();
+				break;
+			case Attacking_State:
+				attack_opponent();
+				break;
+			default:
+				scanForOpponent();
+				break;
+		}
 	}
 }
 
@@ -102,23 +150,25 @@ task displayTask()
 	while(true)
 	{
 		displayTextLine(1,"Color Read: %d", colorRead);
-		displayTextLine(2,"motor B Power: %d", motorB_Power);
-		displayTextLine(3,"motor C Power: %d", motorC_Power);
-		displayTextLine(4,"Range to Obstacle: %d", distanceToObstacle);
+		displayTextLine(2,"current state: %d", current_State);
+		displayTextLine(3,"attack power: %d", Attack_Power);
+		displayTextLine(4,"Range to Obstacle: %f", distanceToObstacle);
 	}
 }
 
 task main()
 {
-	startTask(MotionController);
+	colorRead = 100;
 	startTask(colorSensorTask);
-	startTask(gyroDegreeReader);
+	startTask(gyroRatingReader);
 	startTask(RangeSensorTask);
 	startTask(displayTask);
-	startTask(StateController);
-
-	while(true)
-	{
-		sleep(2000);
+	startTask(colorStateController);
+	startTask(distanceStateController);
+	startTask(squigglyMotionController);
+	//startTask(attackPowerController);
+	startTask(MotionController);
+	while(true){
+		sleep(1000);
 	}
 }
